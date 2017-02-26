0542b270886f80c3f8b994b3f0907d531963e82f
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-02-25 18:35:26 -0600

    FileManager doc

--- cs551/FileManager.h
+++ cs551/FileManager.h
@@ -16,16 +16,34 @@
 using namespace std;
 
 class FileManager {
-public: 
-    
+public:
+
+    /**
+     * Construct a FileManager instance
+     */
     FileManager();
 
+    /**
+     * Destroy a FileManager instance
+     */
     ~FileManager();
 
+    /**
+     * Add at the end of a file the text represented as a vector a string (a string is a line)
+     * @return True if the append succeed else false
+     */
     bool appendToFile(string filePath, vector<string> textToAppend);
 
+    /**
+     * Replace the file content by the text represented as a vector a string (a string is a line)
+     * @return True if the replace succeed else false
+     */
     bool replaceFileContent(string filePath, vector<string> newFileContent);
 
+    /**
+     * Read a file content and returns the text represented as a vector a string (a string is a line)
+     * @return A vector of string containing the file content (can be empty if the file is empty or cannot be read)
+     */
     vector<string> readFileToString(string filePath);
 
 private:
