4983300aee7fef6c803bd6e4a30f1d3db99572a7
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-02-27 19:05:07 -0600

    pdf

--- cs551/Command.cpp
+++ cs551/Command.cpp
@@ -156,16 +156,16 @@ void Command::executeWithExecve() {
 #ifdef DEBUG
         cout << "pid is " << pid << endl;
 #endif
+        const char * fileName = generateFileName();
+        char *const * generatedParams = generateParams();
+        char *const * generatedEnv = generateEnv();
+        cout <<"Executed fileName "<<fileName << endl;
         if (alarmEnabled) {
             close(pipefd[0]);    // close reading end in the child
             dup2(pipefd[1], 1);  // send stdout to the pipe
             dup2(pipefd[1], 2);  // send stderr to the pipe
             close(pipefd[1]);
         }
-        const char * fileName = generateFileName();
-        char *const * generatedParams = generateParams();
-        char *const * generatedEnv = generateEnv();
-        cout <<"Executed fileName "<<fileName << endl;
         int i = execve(fileName, generatedParams, generatedEnv);
 #ifdef DEBUG
         cout << "End of execve with code " << i << endl;
